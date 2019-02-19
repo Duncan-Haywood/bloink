f2e476ef13573689971f6e954e3d88ae06ccbf1d
mvaughan <magnus.vaughan@gmail.com>
2019-02-19 19:41:29 +0000

    Got test tone working with Tone.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import Tone from 'tone';
 
-export default () => <div>Hello world!</div>
+class Welcome extends React.Component {
+    render() {
+
+    //create a synth and connect it to the master output (your speakers)
+    var synth = new Tone.Synth().toMaster();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease('C4', '8n');
+
+    console.log("Innit");
+
+      return (
+        <div className="welcome">
+          <h1>Bragi</h1>
+        </div>
+      );
+    }
+  }
+
+export default Welcome;
