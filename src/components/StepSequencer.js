ea8c7b4703aa8efd272c26aa57aa74778ab5c669
Magnus Vaughan <magnusvaughan@Magnuss-MacBook-Air.local>
2019-02-24 17:04:50 +0000

    Added background image

--- src/components/StepSequencer.js
+++ src/components/StepSequencer.js
@@ -19,9 +19,22 @@ class StepSequencer extends React.Component {
 
     playSequence = () => {
 
-        var synth = new Tone.FMSynth();
-        synth.oscillator.type = 'triangle';
-        synth.toMaster();
+        this.setState({activeStep: 0});
+
+        const synth = new Tone.Synth({
+            oscillator: {
+              type: 'fmsquare',
+              modulationType: 'sawtooth',
+              modulationIndex: 3,
+              harmonicity: 3.4
+            },
+            envelope: {
+              attack: 0.001,
+              decay: 0.1,
+              sustain: 0.1,
+              release: 0.1
+            }
+          }).toMaster()
 
         const $rows = document.querySelectorAll('.row');
         let index = 0;
